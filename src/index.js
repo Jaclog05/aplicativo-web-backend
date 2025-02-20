0b385a37e800ad4b6b2930c0aa85aed63e5532d7
Jaider <jaidercueto.1010@gmail.com>
2025-02-20 12:43:50 -0500

    Connecting with database when running express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const morgan = require('morgan')
-const questionsRoutes = require('./routes/questions.routes')
+import app from './app.js';
+import morgan from 'morgan';
+import sequelize from './database/database.js';
+import './models/Question.js';
+import './models/Appraisal.js';
+import './models/User.js';
+import './models/SquareMeterPrice.js';
 
-const app = express();
-app.use(morgan('dev'))
-app.use(questionsRoutes);
+async function main() {
+  try {
+    await sequelize.sync()
+    app.use(morgan('dev'))
+    app.listen(4000)
+    console.log('listen on port 4000')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-app.listen(4000)
-
-console.log('listen on port 4000')
+main();
