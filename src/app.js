929e04dc253d52051ecbfb10c5235f239cc9411c
Jaider <jaidercueto.1010@gmail.com>
2025-03-03 08:41:30 -0500

    Installing cors. Filtering Unifamiliar questions. Solving id problem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import questionRoutes from './routes/questions.routes.js';
 import appraisalRoutes from './routes/appraisals.routes.js';
 import squareMeterPriceRoutes from './routes/square_meter_prices.routes.js'
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json())
 app.use(questionRoutes)
--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,13 +1,25 @@
 import { Question } from '../models/Question.js';
+import { Op } from 'sequelize';
 
 export const getQuestions = async (req, res) => {
   try {
-    const questions = await Question.findAll()
-    res.json(questions)
+    const { type } = req.query;
+    const whereCondition = type === 'Unifamiliar' 
+      ? { subgroup: { [Op.ne]: "Criterios Especificos" } } 
+      : {};
+
+    const questions = await Question.findAll({ where: whereCondition });
+
+    const questionsWithNewIds = questions.map((question, index) => ({
+      ...question.toJSON(),
+      id: index + 1
+    }))
+
+    res.json(questionsWithNewIds);
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 export const createQuestions = async (req, res) => {
   try {
