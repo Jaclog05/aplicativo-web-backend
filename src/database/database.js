9115c44521fec27de48a0c5a765e8d9e46ac2821
Jaider <jaidercueto.1010@gmail.com>
2025-03-07 09:20:50 -0500

    Connection with deployed database in neon

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,16 @@
 import Sequelize from "sequelize";
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize("appraisals_db", "postgres", "password", {
-  host: "localhost",
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
 });
 
 export default sequelize;
