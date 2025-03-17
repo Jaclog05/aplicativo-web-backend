8ab6aa0023b1d84a9b70761f727bc0f40c12a769
Jaider <jaidercueto.1010@gmail.com>
2025-03-17 13:50:55 -0500

    Adding update square meter prices endpoint

--- src/controllers/square-meter-prices.controller.js
+++ src/controllers/square-meter-prices.controller.js
@@ -9,7 +9,7 @@ export const getSquareMeterPrices = async (req, res) => {
     if (status) whereCondition.status = status;
 
     const squareMeterPrices = await SquareMeterPrice.findAll({ where: whereCondition });
-    res.json(squareMeterPrices[0])
+    res.json(squareMeterPrices)
   } catch (error) {
     res.status(500).json({error: error.message})
   }
@@ -27,3 +27,27 @@ export const createSquareMeterPrices = async (req, res) => {
   }
 }
 
+export const updateSquareMeterPrices = async (req, res) => {
+  try {
+    const squareMeterPrices = req.body;
+
+    if(!Array.isArray(squareMeterPrices) || squareMeterPrices.length === 0) {
+      return res.status(400).json({error: "Invalid data format" });
+    }
+
+    for (const price of squareMeterPrices) {
+      const {id, price: newPrice } = price;
+
+      if(!id || newPrice === undefined) {
+        return res.status(400).json({ error: "Missing required fields"})
+      }
+
+      await SquareMeterPrice.update({ price: newPrice }, {where: {id}})
+    }
+
+    res.json({ message: "Square meter prices updated successfully" })
+
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
--- src/routes/square_meter_prices.routes.js
+++ src/routes/square_meter_prices.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getSquareMeterPrices, createSquareMeterPrices } from "../controllers/square-meter-prices.controller.js";
+import {
+  getSquareMeterPrices,
+  createSquareMeterPrices,
+  updateSquareMeterPrices,
+} from "../controllers/square-meter-prices.controller.js";
 
 const router = Router();
 
-router.get('/square-meter-prices', getSquareMeterPrices)
-router.post('/square-meter-prices', createSquareMeterPrices)
+router.get("/square-meter-prices", getSquareMeterPrices);
+router.post("/square-meter-prices", createSquareMeterPrices);
+router.put('/square-meter-prices', updateSquareMeterPrices);
 
 export default router;
