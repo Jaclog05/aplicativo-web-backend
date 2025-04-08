138b0377f983b964652dd2967ecb64834feee526
Jaider <jaidercueto.1010@gmail.com>
2025-04-08 11:28:23 -0500

    Changing admin user credentials. Using crypto

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,18 @@
-const {ADMIN_USER, ADMIN_PASSWORD, SECRET_KEY} = process.env
+const {ADMIN_USER, ADMIN_PASSWORD_HASH, SECRET_KEY} = process.env
 
 import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+
+function hashPassword(password) {
+  return crypto.createHash('sha256').update(password).digest('hex');
+}
 
 export const loginAdminUser = async (req, res) => {
 
   const { user, password } = req.body;
+  const hashedPassword = hashPassword(password)
 
-  if (user === ADMIN_USER && password === ADMIN_PASSWORD) {
+  if (user === ADMIN_USER && hashedPassword === ADMIN_PASSWORD_HASH) {
     const token = jwt.sign({ user }, SECRET_KEY, { expiresIn: "2h" });
     return res.json({ token });
   }
